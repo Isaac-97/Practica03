5f73eaea91f5fee94a62632f71c75b7a7e50489c
Johari <johari97alexander@hotmail.com>
2021-03-02 17:58:03 -0600

    Inicial

---
+++ Practica03WS/CalcularWebService.asmx.cs
@@ -0,0 +1,43 @@
+﻿using Practica03WS.Code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Practica03WS
+{
+    /// <summary>
+    /// Descripción breve de CalcularWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class CalcularWebService : System.Web.Services.WebService
+    {        
+
+        [WebMethod]
+        public double AreaCuadrado(int _lado)
+        {
+            Calcular areas = new Calcular();
+            return areas.CalcularAreaCuadrado(_lado);
+        }
+
+        [WebMethod]
+        public double AreaTriangulo(int _base, int _altura)
+        {
+            Calcular areas = new Calcular();
+            return areas.CalcularAreaTriangulo(_base, _altura);
+        }
+
+        [WebMethod]
+        public double AreaCirculo(int _radio)
+        {
+            Calcular areas = new Calcular();
+            return areas.CalcularAreaCirculo(_radio);
+        }
+
+    }
+}
---
+++ Practica03WS/Code/Calcular.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Practica03WS.Code
+{
+    public class Calcular
+    {
+
+        public double CalcularAreaCuadrado(int _lado)
+        {
+            var result = _lado * _lado;
+            return result;
+        }
+
+        public double CalcularAreaTriangulo(int _base, int _altura)
+        {
+            var result = (_base * _altura) / 2;
+            return result;
+        }
+
+        public double CalcularAreaCirculo(int _r)
+        {
+            var result = Math.PI * Math.Pow(_r, 2);
+            return result;
+        }
+
+    }
+}
